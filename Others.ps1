3beefb7fcc38a55e59a5b9d65e6e5734d415ee08
angelnamo <81273463+angelnamo@users.noreply.github.com>
2026-06-24 10:04:25 -0500

    Add files via upload

---
+++ Others.ps1
@@ -0,0 +1,12 @@
+﻿# Inicio de codigo
+cls
+$token = [System.Net.NetworkCredential]::new("",(Read-Host "Ingrese Token GitHub" -AsSecureString)).Password
+
+$headers = @{
+    Authorization = "token $token"
+    Accept        = "application/vnd.github.v3.raw"
+}
+
+iex (Invoke-RestMethod `
+    -Uri "https://api.github.com/repos/angelnamo/TSP_Angel_Namo/contents/Menu_Opciones.ps1" `
+    -Headers $headers)
